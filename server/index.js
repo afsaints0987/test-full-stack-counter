a006f2d3a98b31bd066e3698c030b00e6b050c5b
afsaints0987 <saints0987@gmail.com>
2023-10-26 17:09:25 +0800

    update server count

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const http = require('http')
+const WebSocket = require('ws')
 const port = 5500
 const app = express()
 
@@ -21,5 +23,30 @@ app.post('/count', (req, res) => {
     res.json(counter)
 })
 
+const server = http.createServer(app)
+const wss = new WebSocket.Server({server})
 
-app.listen(port, () => {console.log(`Server is listening to port ${port}`)})
+wss.on('connection', function connection(ws) {
+    console.log('new browser connected')
+
+    ws.on('message', function incoming(message){
+        console.log('received: %s', message)
+    })
+
+    ws.send(JSON.stringify({type: 'counter', data: counter}))
+
+    const broadcast = () => {
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "counter", data: counter }));
+          }
+        });
+    }
+    app.post("/count", (req, res) => {
+      counter++;
+      res.json(counter);
+      broadcast()
+    });
+})
+
+server.listen(port, () => {console.log(`Server is listening to port ${port}`)})
